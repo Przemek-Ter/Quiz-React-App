6d9a77bc0ecb89199b6908b72aa8366ee0cd716f
Przemysław Terech <57288321+Przemek-Ter@users.noreply.github.com>
2020-06-22 13:28:11 +0200

    Add files via upload

---
+++ QuizReact/build/build/precache-manifest.a938a4842d9c73ce5e2a1238fa3439b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14aaf5681ceb8582a7e9ed386d1a8e07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8962d188283c0d545832",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "4103f410e6af27337b93",
+    "url": "/static/js/2.7133b9c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7133b9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8962d188283c0d545832",
+    "url": "/static/js/main.68fc420f.chunk.js"
+  },
+  {
+    "revision": "3b19388fce55e302d594",
+    "url": "/static/js/runtime-main.68cae411.js"
+  }
+]);
